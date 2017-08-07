072bf24efac4766c2f7de73752431d6753eee3e0
毕晓朕 <bixiaozhen@bixiaozhendeMacBook-Pro.local>
2017-08-07 20:14:32 +0800

    ru

--- pre_process_ru.py
+++ pre_process_ru.py
@@ -3,10 +3,23 @@ from coda.tokenizer import Tokenizer
 from coda.disambiguator import Disambiguator
 from coda.syntax_parser import SyntaxParser
 import time
+import nltk
 
 dic = {}
 tokenizer = Tokenizer("RU")
 disambiguator = Disambiguator("RU")
+
+class DotDict(dict):
+    def __init__(self):
+        dict.__init__(self)
+        self.__dict__ = self
+
+    def allow_dotting(self, state=True):
+        if state:
+            self.__dict__ = self
+        else:
+            self.__dict__ = dict()
+
 
 class pre_process(object):
     def __init__(self, punctuation_file, stopwords_file, special_terms=["_line_"]):
@@ -15,6 +28,19 @@ class pre_process(object):
         self.special_terms = special_terms
         self.__tokens = None
         self.__content = []
+        self.grammars = ["NP: {<N.*>}", "NP: {<A|N.*><N.*>}", "NP: {<N.*><IN|N.*><N.*>}"]
+
+    def __to_unicode(self,input_str):
+        """
+        确定编码
+        :param input_str:
+        :return:
+        """
+        if isinstance(input_str, str):
+            return unicode(input_str, encoding="utf-8", errors='ignore')
+        else:
+            return input_str
+
     def __del_special_terms(self, text):
         """
         删除指定特殊字符。
@@ -70,6 +96,29 @@ class pre_process(object):
         for item in disambiguated:
             self.__content.append((item.lemma,item.label.split('@')[0],item.content))
 
+    def __get_noun_chunks(self):
+        """
+        得到名词组合
+        :return:
+        """
+        tagged_words = [(w[2],w[0]) for w in self.__content]
+        noun_chunks = []
+        for grammar in self.grammars:
+            cp = nltk.RegexpParser(grammar)
+            result = cp.parse(tagged_words)
+            NP = [list(kt) for kt in result if not isinstance(kt, tuple)]
+            NP = [" ".join([w for w, _ in np]) for np in NP]
+            noun_chunks += NP
+        return noun_chunks
+
+    def __get_ents(self):
+        """
+        得到实体词
+        :return:
+        """
+        return []
+
+
     def text_process(self, text):
         """
         文本预处理。
@@ -86,11 +135,33 @@ class pre_process(object):
         #terms = self.__stem(self.__is_local_for_text(terms))
         return [w[0] for w in self.__content]
 
+    def __call__(self, text):
+        """
+        关键词接口
+        :param text:
+        :return:
+        """
+        self.__content = []
+        text = self.__del_punctution(self.__del_special_terms(text.lower()))
+        if text.strip() == '':
+            return []
+        self.__tokenize(text)
+        self.__stem()
+        term = DotDict()
+        term.allow_dotting()
+        term.noun_chunks = self.__get_noun_chunks()
+        term.ents = self.__get_ents()
+
+
+
 
 if __name__ == '__main__':
     pp = pre_process("punctuation.txt", "stopwords.txt")
     start_time = time.time()
-    print pp.text_process(u'МИД пригрозил ограничить поездки американских дипломатов по России.')
+    text = u'МИД пригрозил ограничить поездки американских дипломатов по России.'
+    print pp.text_process(text)
+    doc = pp(text)
+    print doc.noun_chunks
 
 
 
